0ef13311b6e528b693811eba2eb4b6f23e350ec1
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-10-27 20:24:14 +0800

    Commit 2 - Forecast 5 days

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
 import axios from "axios";
@@ -13,6 +13,7 @@ const App = () => {
   const [currWeatherDes, setCurrWeatherDes] = useState("");
   const [currWeatherIcon, setCurrWeatherIcon] = useState("");
   const [checked, setChecked] = useState(false);
+  const [dataForecast, setDataForecast] = useState({});
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -40,8 +41,45 @@ const App = () => {
         setCurrWeatherDes(weatherData.weather[0].description);
         setCurrWeatherIcon(weatherData.weather[0].icon);
       });
+    axios
+      .get(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${OPEN_WEATHER_API_KEY}`,
+      )
+      .then((response) => response.data[0])
+      .then((cityGeoData) =>
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`,
+        ),
+      )
+      .then((response) => {
+        const { data: forecastData } = response;
+        console.log(forecastData);
+        setDataForecast(forecastData);
+      });
     setChecked(true);
   };
+
+  let result;
+
+  const forecast = () =>
+    (result = dataForecast.list.map((data) => (
+      <p>
+        <img
+          src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+          alt="weather-icon"
+        />
+        <br />
+        Weather: {data.weather[0].main}
+        <br />
+        Date: {data.dt_txt}
+        <br />
+        <br />
+      </p>
+    )));
+
+  if (Object.keys(dataForecast).length > 0) {
+    forecast();
+  }
 
   return (
     <div className="App">
@@ -69,7 +107,13 @@ const App = () => {
             />
             <p>Current City: {currCity}</p>
             <p>Current Temp: {currTemp} °C</p>
-            <p>Current Weather: {currWeather}, {currWeatherDes}</p>
+            <p>
+              Current Weather: {currWeather}, {currWeatherDes}
+            </p>
+            <br />
+            <br />
+            <p>Forecast for the next 5 days</p>
+            <div className="container">{result}</div>
           </div>
         )}
       </header>

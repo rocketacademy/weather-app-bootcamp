397a8122fa7c2e5f661752b370e1baf780e927f4
mafiageek <anton_kho@msn.com>
2022-10-28 20:24:32 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Container } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container maxWidth="sm">
+      <App />
+    </Container>
   </React.StrictMode>
 );

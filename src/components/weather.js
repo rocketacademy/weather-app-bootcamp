181617642f1d50f3fbd55f363be4b1c810ece170
Nicklye26 <nicklye26@gmail.com>
2022-10-28 19:35:37 +0800

    corrected the open weather api key

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 
+const OPEN_WEATHER_API_KEY = "3540653b10b634e1bdacc26d27f1e270";
+
 export default class Weather extends React.Component {
   constructor(props) {
     super(props);
@@ -28,14 +30,14 @@ export default class Weather extends React.Component {
     event.preventDefault();
     axios
       .get(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${this.state.cityInputValue}&limit=1&appid=3540653b10b634e1bdacc26d27f1e270`,
+        `https://api.openweathermap.org/geo/1.0/direct?q=${this.state.cityInputValue}&limit=1&appid=${OPEN_WEATHER_API_KEY}`,
       )
       // City geo data is in response.data[0]
       // Arrow functions with no curly braces return value after arrow
       .then((response) => response.data[0])
       .then((cityGeoData) =>
         axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=3540653b10b634e1bdacc26d27f1e270&units=metric`,
+          `https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`,
         ),
       )
       .then((response) => {

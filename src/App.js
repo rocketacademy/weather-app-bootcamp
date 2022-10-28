5782c4e453bcd36298ad0155b8da982ff166c055
sharlearn <liewsharlen@gmail.com>
2022-10-29 00:12:51 +0800

    Added 5 day Forecast Table

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
       weather: {},
       weatherIcon: "",
       coordinates: { lat: "", lon: "" },
+      forecastWeather: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -31,10 +32,11 @@ class App extends React.Component {
       // City geo data is in response.data[0]
       // Arrow functions with no curly braces return value after arrow
       .then((response) => {
-        console.log(response);
+        this.setState({
+          coordinates: { lat: response.data[0].lat, lon: response.data[0].lon },
+        });
         return response.data[0];
       })
-      /* .then((response) => response.data[0]) */
       .then((cityGeoData) =>
         axios.get(
           `https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid=b19b32221939a66675b01f5452af913c&units=metric`
@@ -42,29 +44,63 @@ class App extends React.Component {
       )
       .then((response) => {
         const { data: weatherData } = response;
+        console.log(this.state.coordinates);
         this.setState({
           city: this.state.cityInputValue,
           weather: weatherData.main,
           weatherIcon: weatherData.weather[0],
           cityInputValue: "",
         });
+      })
+      .then(() =>
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.coordinates.lat}&lon=${this.state.coordinates.lon}&appid=b19b32221939a66675b01f5452af913c&units=metric`
+        )
+      )
+      .then((response) => {
+        console.log(response.data.list);
+        this.setState({ forecastWeather: response.data.list });
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
   render() {
+    console.log(this.state.forecastWeather);
+    const forecastedWeather = this.state.forecastWeather;
     const weatherInfo = (
       <div>
-        <h4>{this.state.city}</h4>
-        <img
-          src={`http://openweathermap.org/img/wn/${this.state.weatherIcon.icon}@2x.png`}
-          alt={`${this.state.weatherIcon.main}`}
-        />
-        <p>Today's Weather: {this.state.weatherIcon.main}</p>
-        <p>Current Temperature: {this.state.weather.temp}</p>
-        <p>Feels Like: {this.state.weather.feels_like}</p>
-        <p>Low: {this.state.weather.temp_min}</p>
-        <p>High: {this.state.weather.temp_max}</p>
-        <p>Humidity: {this.state.weather.humidity}</p>
+        <div>
+          <h4>{this.state.city}</h4>
+          <img
+            src={`http://openweathermap.org/img/wn/${this.state.weatherIcon.icon}@2x.png`}
+            alt={`${this.state.weatherIcon.main}`}
+          />
+          <p>Today's Weather: {this.state.weatherIcon.main}</p>
+          <p>Current Temperature: {this.state.weather.temp}</p>
+          <p>Feels Like: {this.state.weather.feels_like}</p>
+          <p>Low: {this.state.weather.temp_min}</p>
+          <p>High: {this.state.weather.temp_max}</p>
+          <p>Humidity: {this.state.weather.humidity}</p>
+        </div>
+        <h4>5 Day Forecast</h4>
+        {forecastedWeather.map((forecasts) => (
+          <table>
+            <tr>
+              <th>Date</th>
+              <th>Temp_Min</th>
+              <th>Temp_Max</th>
+              <th>Weather</th>
+            </tr>
+            <tr>
+              <td>{forecasts.dt_txt}</td>
+              <td>{forecasts.main.temp_min}</td>
+              <td>{forecasts.main.temp_max}</td>
+              <td>{forecasts.weather[0].description}</td>
+            </tr>
+          </table>
+        ))}
       </div>
     );
 

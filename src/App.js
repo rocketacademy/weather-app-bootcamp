dd93c36a1d106229a9a920dcc1dd3d72f36d5f2a
NTU_COSMO <yuhao.huang@ntu.edu.sg>
2023-05-19 17:09:47 +0800

    added weather and date

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inputValue: "" };
+    this.state = { inputValue: "", cityName: "", country: "", weather: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -13,28 +13,45 @@ class App extends React.Component {
     this.setState({ inputValue: event.target.value });
   }
   handleSubmit(event) {
-    //alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
     const { inputValue } = this.state;
+    // let latitude = 0;
+    // let longtitude = 0;
     if (
       inputValue === "" ||
       inputValue.toUpperCase() === inputValue.toLowerCase()
     ) {
       alert("Please enter a proper city name first.");
     } else {
-      console.log("testing")
-      // axios
-      //   .get(
-      //     `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=`
-      //   )
-      //   .then((response) => {
-      //     console.log(response);
-      //     return <div>{response}</div>
-        
-      //   });
+      axios
+        .get(
+          `https://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=1&appid={API key}`
+        )
+
+        .then((response) => response.data[0])
+        .then((cityGeoData) =>
+          axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${cityGeoData.lat}&lon=${cityGeoData.lon}&appid={API key}&units=metric`
+          )
+        )
+        .then((response) => {
+          const { data: weatherData } = response;
+          console.log(weatherData.weather[0].main);
+          const cityName = weatherData.name;
+          const country = weatherData.sys.country;
+          const weather = weatherData.weather[0].main;
+          this.setState({
+            cityName,
+            country,
+            weather,
+          });
+        });
     }
   }
+
   render() {
+    const { cityName, country,weather } = this.state;
+    const date = new Date();
     return (
       <div className="App">
         <header className="App-header">
@@ -50,6 +67,10 @@ class App extends React.Component {
             </label>
             <input type="submit" value="Submit" />
           </form>
+          <div>
+            The weather at {cityName}, {country} on{" "}
+            {date.toLocaleDateString("en-GB")} is {weather}
+          </div>
         </header>
       </div>
     );

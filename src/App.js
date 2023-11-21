9d405297be80825259cd70c92f9548ede1e52c9d
patrickkok <pkok1996@gmail.com>
2023-11-21 22:21:44 +0800

    Add input that calls API on submit to display weather. Submitting another city adds to the weather display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import axios from "axios";
+const apiKey = "6551f6311cfd170cad474dd24a9b6e05";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { city: "", weather: [] };
+  }
+
+  getWeather = async () => {
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${apiKey}`
+      )
+      .then((response) =>
+        this.setState({
+          weather: [...this.state.weather, response.data],
+        })
+      );
+    return true;
+  };
+
+  handleChange = (e) => {
+    this.setState({ city: `${e.target.value}` });
+  };
+
+  handleSubmit = () => {
+    this.getWeather(this.state.city);
+  };
+
   render() {
+    const weatherDisplay = this.state.weather.map((obj) => (
+      <div key={obj.name}>
+        <h1>{obj.name}</h1>
+        <img
+          src={`https://openweathermap.org/img/wn/${obj.weather[0].icon}.png`}
+          alt={obj.weather[0].main}
+        />
+        <p>{obj.weather[0].description}</p>
+      </div>
+    ));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <input
+          type="text"
+          value={this.state.city}
+          placeholder="City"
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input type="submit" value="submit" onClick={this.handleSubmit} />
+        {this.state.weather && this.state.weather.length > 0
+          ? weatherDisplay
+          : null}
       </div>
     );
   }

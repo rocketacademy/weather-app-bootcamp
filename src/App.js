c474377bd2ffcf9ae5ad5acb910897284c10b333
magiicloud <hongyun_rc@outlook.com>
2023-11-22 22:29:40 +0800

    amended the API, defined weatherData variable and removed consolelog for state

--- src/App.js
+++ src/App.js
@@ -16,11 +16,12 @@ class App extends React.Component {
 
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid="6a3affea0dc3f2287d470f99deb8e2f2"`
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=6a3affea0dc3f2287d470f99deb8e2f2`
       )
 
       .then((response) => {
-        console.log(response);
+        const weatherData = response.data["weather"];
+        console.log(weatherData);
       });
   };
 
@@ -31,26 +32,25 @@ class App extends React.Component {
   };
 
   render() {
-    console.log(this.state.city);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              City
+          <img src={logo} className="App-logo rounded-xl" alt="logo" />
+          <div className="p-2 m-2">
+            <form onSubmit={this.handleSubmit}>
+              <p className="p-2 mb-1">Search the city you desire</p>
               <input
+                className="m-2 px-2 py-1 text-black rounded-lg"
                 type="text"
                 value={this.state.value}
                 onChange={this.handleChange}
                 placeholder="Enter city name"
               />
-              <input type="submit" value="Submit" />
-            </label>
-          </form>
+              <button className="px-2 py-1 bg-indigo-600 hover:bg-indigo-700 rounded-lg">
+                Submit
+              </button>
+            </form>
+          </div>
         </header>
       </div>
     );

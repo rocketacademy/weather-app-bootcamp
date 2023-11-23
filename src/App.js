6b358db2bc96238a4c3904f68933bda8cce5f583
magiicloud <hongyun_rc@outlook.com>
2023-11-23 21:56:32 +0800

    deleted commented out and unused code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.png";
-import axios from "axios";
 import "./App.css";
 import WeatherInput from "./components/WeatherInput";
 import WeatherAPI from "./weatherAPI";
@@ -31,31 +30,6 @@ class App extends React.Component {
     });
   };
 
-  //   axios
-  //     .get(
-  //       `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&appid=6a3affea0dc3f2287d470f99deb8e2f2`
-  //     )
-
-  //     .then((response) => {
-  //       console.log(response);
-  //       const weatherData = response.data["weather"];
-  //       const temperatureData = response.data["main"];
-  //       this.setState({
-  //         initialRender: false,
-  //         temperature: temperatureData["temp"],
-  //         icon: weatherData[0]["icon"],
-  //       });
-  //     });
-
-  //   axios
-  //     .get(
-  //       `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.city}&units=metric&appid=6a3affea0dc3f2287d470f99deb8e2f2`
-  //     )
-  //     .then((response) => {
-  //       console.log(response);
-  //     });
-  // };
-
   handleChange = (e) => {
     this.setState({
       city: e.target.value,
@@ -68,30 +42,6 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo rounded-xl" alt="logo" />
-          {/* <div className="p-2 m-2">
-            <form onSubmit={this.handleSubmit}>
-              <p className="p-2 mb-1">Search the city you desire</p>
-              <input
-                className="m-2 px-2 py-1 text-white rounded-lg"
-                type="text"
-                value={this.state.value}
-                onChange={this.handleChange}
-                placeholder="Enter city name"
-              />
-              <button className="px-2 py-1 bg-indigo-600 hover:bg-indigo-700 rounded-lg">
-                Submit
-              </button>
-            </form>
-          </div>
-          <div>
-            {this.state.icon !== "" && (
-              <img
-                src={`https://openweathermap.org/img/wn/${this.state.icon}.png`}
-                alt="weathericon"
-              />
-            )}
-            {!this.state.initialRender && <h3>{this.state.temperature}°C</h3>}
-          </div> */}
           <WeatherInput
             handleSubmit={this.handleSubmit}
             icon={this.state.icon}
--- src/components/WeatherInput.js
+++ src/components/WeatherInput.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 class WeatherInput extends React.Component {
   constructor(props) {

50f64411421d3d508f6cc5d3820e59c7eddb7707
qiannnly <qianlinggg@gmail.com>
2023-11-26 11:14:53 +0800

    added states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from "react";
-import logo from "./logo.png";
+import React, { useState } from "react";
 import "./App.css";
+import { getWeather } from "./utils";
+import { Weather } from "./components/Weather";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [cityName, setCityName] = useState("");
+  const [weather, setWeather] = useState({
+    city: "",
+    weatherInfo: "",
+    temp: "",
+    feelsLike: "",
+    humidity: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeather(cityName).then((weatherData) => {
+      setWeather({
+        city: weatherData.name,
+        weatherInfo: weatherData.weather[0],
+        temp: weatherData.main.temp,
+        feelsLike: weatherData.main.feels_like,
+        humidity: weatherData.main.humidity,
+      });
+    });
+  };
+  console.log(weather);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a city"
+            onChange={(e) => setCityName(e.target.value)}
+            value={cityName}
+          />
+          <button type="submit">Get weather</button>
+        </form>
+        <Weather weatherResult={weather} />
+      </header>
+    </div>
+  );
+};
 
 export default App;
